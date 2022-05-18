c4639fca47ff8ddaa4e7e6fcf9df013531a97454
LeMinh22621 <lehongminh22621@gmail.com>
2022-05-18 10:07:12 +0700

    create app bar

---
+++ lib/app_colors.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+final Color background = Color(0xFFfafafc);
+final Color sliverBackground = Color(0xFFfafafc);
+final Color menu1Color = Color(0xFFf9d263);
+final Color menu2Color = Color(0xFFf2603d);
+final Color menu3Color = Color(0xFF04abe6);
+final Color tabVarViewColor = Color(0xFFfdfdfd);
+final Color starColor = Color(0xFFfdc746);
+final Color subTitleText = Color(0xFFc5c4ca);
+final Color loveColor = Color(0xFF00ace6);
+final Color audioBlueBackground = Color(0xFFeeeffa);
+final Color audioGreyBackground = Color(0xFFf2f2f2);
